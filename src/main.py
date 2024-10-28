e29748c9e933acef5171f9164043f5d1e638b07c
VC <vicu.ciobu@gmail.com>
2024-10-28 17:12:29 -0400

    Add customtkinter and create requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
-from tkinter import *
-from PIL import ImageTk, Image
+#import tkinter
+import customtkinter
+from numba import jit
+#from PIL import ImageTk, Image
 
-root = Tk()
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+root = customtkinter.CTk()
 
 root.title("GOOGOL")
 root.geometry("600x400")
-root.wm_iconphoto(True, ImageTk.PhotoImage(Image.open("assets/icon.png")))
+#root.wm_iconphoto(True, ImageTk.PhotoImage(Image.open("assets/icon.png")))
 
 if __name__ == "__main__":
     root.mainloop()
