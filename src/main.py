02abce4261d8f813c73c04f191d847962bded689
VC <vicu.ciobu@gmail.com>
2024-10-28 20:03:25 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 #import tkinter
 import customtkinter
 from numba import jit
-#from PIL import ImageTk, Image
+from PIL import ImageTk, Image
 
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
 root = customtkinter.CTk()
 
-root.title("GOOGOL")
+root.title("Googol")
 root.geometry("600x400")
 #root.wm_iconphoto(True, ImageTk.PhotoImage(Image.open("assets/icon.png")))
 
